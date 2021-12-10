44be9d35483cd0691aaba3deeef1a905be81fc41
Chukwuemeka Momodu <momodu.ceo@gmail.com>
2021-12-10 05:21:04 +0100

    bug fixes on trial up

--- reference-app/trial/app.py
+++ reference-app/trial/app.py
@@ -1,44 +1,30 @@
-import requests
 from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
+import logging, requests
 
 from jaeger_client import Config
 from jaeger_client.metrics.prometheus import PrometheusMetricsFactory
 from opentelemetry import trace
+from opentelemetry.exporter.jaeger.thrift import JaegerExporter
 from opentelemetry.exporter import jaeger
 from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchExportSpanProcessor
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
 from opentelemetry.instrumentation.requests import RequestsInstrumentor
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import (
-    ConsoleSpanExporter,
-    SimpleExportSpanProcessor,
-)
-import logging
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.sdk.trace.export import ConsoleSpanExporter
 from prometheus_flask_exporter.multiprocess import GunicornInternalPrometheusMetrics
+from prometheus_flask_exporter import PrometheusMetrics
 
+'''
 trace.set_tracer_provider(TracerProvider())
 trace.get_tracer_provider().add_span_processor(
     SimpleExportSpanProcessor(ConsoleSpanExporter())
 )
-
-app = Flask(__name__)
-FlaskInstrumentor().instrument_app(app)
-RequestsInstrumentor().instrument()
-
-metrics = GunicornInternalPrometheusMetrics(app)
-# static information as metric
-metrics.info('trial_app_info', 'Trial Application info', version='1.0.0')
+'''
 
 
-# config = Config(
-#        config={},
-#        service_name='your-app-name',
-#        validate=True,
-#        metrics_factory=PrometheusMetricsFactory(service_name_label='your-app-name')
-# )
-# tracer = config.initialize_tracer()
-
+# Configure Jaeger tracer
 def init_tracer(service):
     logging.getLogger('').handlers = []
     logging.basicConfig(format='%(message)s', level=logging.DEBUG)
@@ -52,16 +38,51 @@ def init_tracer(service):
             'logging': True,
         },
         service_name=service,
+        validate=True
     )
 
     # this call also sets opentracing.tracer
     return config.initialize_tracer()
 
 
-tracer = init_tracer('first-service')
+tracer = init_tracer('trial')
 
+# Trial App
+app = Flask(__name__)
+FlaskInstrumentor().instrument_app(app)
+RequestsInstrumentor().instrument()
+CORS(app)
+
+# add this missing from starter code
+metrics = GunicornInternalPrometheusMetrics(app, group_by='endpoint')
+
+# static information as metric
+metrics.info('trial_app', 'Trial App Metrics', version='1.0.0')
+
+# register extra metrics
+metrics.register_default(
+    metrics.counter(
+        'by_path_counter', 'Request count by request paths', labels={'path': lambda: request.path}
+    )
+)
+
+# custom metric to be applied to multiple endpoints
+endpoint_counter = metrics.counter(
+    'by_endpoint_counter', 'Request count by endpoints',
+    labels={'endpoint': lambda: request.endpoint}
+)
+
+
+# config = Config(
+#        config={},
+#        service_name='your-app-name',
+#        validate=True,
+#        metrics_factory=PrometheusMetricsFactory(service_name_label='your-app-name')
+# )
+# tracer = config.initialize_tracer()
 
 @app.route('/')
+@endpoint_counter
 def homepage():
     # return render_template("main.html")
     with tracer.start_span('get-python-jobs') as span:
@@ -73,7 +94,6 @@ def homepage():
                 homepages.append(requests.get(result['company_url']))
             except:
                 return "Unable to get site for %s" % result['company']
-
     return jsonify(homepages)
 
 
